60d4b51bc81c44d4a07aeb77a41471e87674876c
Kuba255 <73470071+Kuba255@users.noreply.github.com>
2021-01-22 15:12:10 +0100

    Add files via upload

---
+++ Tydz8/external3.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+?>
+<div class="main2">
+									
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+    </head>
+    <body>
+        
+        
+        <p class="sign" align="center">Zmiana Hasla</p>
+									<form class="form1" method="POST" action="zmiana-ustawien.php">
+									<input class="un" type="password" align="center" name="StareHaslo" placeholder="Stare Hasło">
+									<br>
+									<input class="pass" type="password" align="center" name="NoweHaslo" placeholder="Nowe Hasło">
+									</br>
+									<input class="pass" type="password" align="center" name="PowtorzNoweHaslo" placeholder="Powtórz Nowe Hasło">
+									<br>
+									<input type="submit" value="Zmień hasło" name="ZmienHaslo" class="submit" align="center">
+									</br>
+									</form>
+									</div>
+        
+        
+        
+        
+    </body>
+    </html>
+    
