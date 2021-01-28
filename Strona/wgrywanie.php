736cd339e8dd2593c3e73c9069153a43618db032
Kuba255 <73470071+Kuba255@users.noreply.github.com>
2021-01-28 17:19:53 +0100

    Update wgrywanie.php

--- Strona/wgrywanie.php
+++ Strona/wgrywanie.php
@@ -10,40 +10,42 @@ $con =mysqli_connect('localhost','id15971773_bazaio20test','JakiesLosoweHaslo12!
 $query = "SELECT TOP16(Nazwa) FROM Obrazy OREDR BY DataWstawienia";
 $data=mysqli_query($con,$query);
 
-
-
-
 if (mysqli_num_rows($data)>0)
 {
   echo "istnieje juz plik o takiej nazwie"; 
 }
 else
-{
-if(is_uploaded_file($plik_tmp))
-{
-    $query3 = "SELECT ID FROM Rejestracja WHERE Login='$login'";
-    $data2=mysqli_query($con,$query3);
-    //$wiersz = $data2->fetch_assoc();
-    $wiersz = mysqli_fetch_array($data2);
-    $wiersz1 = $wiersz['ID'];
-    //echo "test".$wiersz1."  ".$plik_nazwa."  "."  ".$plik_rozmiar."  ".$dataDoda."                 ".$tytul;
-    $con->query("INSERT INTO Obrazy(IDUzytkownika, Nazwa, TytulObrazu, Rozmiar, DataWstawienia, Lapka) VALUES ('$wiersz1', '$plik_nazwa', '$tytul','$plik_rozmiar','$dataDoda','0')");
-    $con->close();
-    
-  //  $con->query("INSERT INTO Rejestracja(Login, Haslo, Email)
-//VALUES ('$login', '$haslo_szyfr', '$email')");
-//$con->close();
-//, '$plik_nazwa', '$tytul','$plik_tmp','$plik_rozmiar','$dataDoda','0'
-//, Nazwa, TytulObrazu, Typ, Rozmiar, DataWstawienia, Lapka
-
-    move_uploaded_file($plik_tmp, "obraz/".$plik_nazwa);
-    echo "Plik: <strong>$plik_nazwa</strong> o rozmiarze
-    <strong>$plik_rozmiar bajtów</strong> został przesłany na serwer!";
-}
-else
-{
-   echo '"Nigdy nie będzie tak, że nie napotkasz problemów" ';
-}
+{   
+    if($plik_rozmiar <= 2000000)
+    {
+        if(strlen($plik_nazwa) <= 25) 
+        {
+            if(is_uploaded_file($plik_tmp))
+            {
+                $query3 = "SELECT ID FROM Rejestracja WHERE Login='$login'";
+                $data2=mysqli_query($con,$query3);
+                $wiersz = mysqli_fetch_array($data2);
+                $wiersz1 = $wiersz['ID'];
+                $con->query("INSERT INTO Obrazy(IDUzytkownika, Nazwa, TytulObrazu, Rozmiar, DataWstawienia, Lapka) VALUES ('$wiersz1', '$plik_nazwa', '$tytul','$plik_rozmiar','$dataDoda','0')");
+                $con->close();
+                move_uploaded_file($plik_tmp, "obraz/".$plik_nazwa);
+                echo "Plik: <strong>$plik_nazwa</strong> o rozmiarze
+                <strong>$plik_rozmiar bajtów</strong> został przesłany na serwer!";
+            }
+            else
+            {
+               echo '"Nigdy nie będzie tak, że nie napotkasz problemów" ';
+            }
+        }
+        else
+        {
+            echo 'Za długa nazwa pliku';
+        }
+    }
+    else
+    {
+        echo 'Za duży rozmiar pliku!';
+    }
 }
 exit();
 ?>
