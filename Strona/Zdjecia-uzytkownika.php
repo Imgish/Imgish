a42e612c2798f67514da7eeecc9106aa1360b9b5
Raviolinii <54943478+Raviolinii@users.noreply.github.com>
2021-01-28 19:05:11 +0100

    Update Zdjecia-uzytkownika.php

--- Strona/Zdjecia-uzytkownika.php
+++ Strona/Zdjecia-uzytkownika.php
@@ -1,12 +1,21 @@
 
-
-
 <html>
+ <link rel="stylesheet" href="css/PicUpload.css">      
  <body>
-  <form action="wgrywanie.php" method="POST" ENCTYPE="multipart/form-data">
-   <input type="file" name="plik"/><br/>
-    <input type="text" name="nazwapliku">
-   <input type="submit" value="Wyślij plik"/>
+    <form action="wgrywanie.php" method="POST" ENCTYPE="multipart/form-data">
+        
+    <div class="PicUploadSpace">
+        
+    <div class="PicChose">
+        <input type="file" name="plik" class="PicSelect"/><br/>
+   </div>
+        
+    <div class="PicSubmission">
+        <input type="text" name="nazwapliku" class="PicName">
+        <input type="submit" value="Wyślij"/ class="PicSubmit">
+    </div>
+    
+   </div>
 
   </form>
  </body>
