f98bfbe441d6b6faac9befc9d4d8c3046e1f38b8
Nyny <59801348+NynyNoo@users.noreply.github.com>
2021-01-29 12:53:21 +0100

    Update Zdjecia-uzytkownika.php

--- Strona/Zdjecia-uzytkownika.php
+++ Strona/Zdjecia-uzytkownika.php
@@ -2,36 +2,64 @@
 <html>
  <link rel="stylesheet" href="css/PicUpload.css">      
  <body>
-    <form action="wgrywanie.php" method="POST" ENCTYPE="multipart/form-data">
-        
-    <div class="PicUploadSpace">
-        
+<style>
+#drop_zone {
+    background-color: #EEE; 
+    border: #999 5px dashed;
+    width: 290px; 
+    height: 200px;
+    padding: 8px;
+    font-size: 18px;
+}
+      
+     
+
+</style>
+<form action="wgrywanie.php" method="POST" ENCTYPE="multipart/form-data">  
     <div class="PicChose">
-        <input type="file" name="plik" class="PicSelect"/><br/>
-   </div>
+        
+        
+        
+        
+        
+<script>
+function drag_drop(event) {
+    event.preventDefault();
+    /*alert(event.dataTransfer.files[0]);
+    alert(event.dataTransfer.files[0].name);
+    alert(event.dataTransfer.files[0].size+" bytes");*/
+    /*  This is where to begin uploading the file with Ajax and upload progress bar to PHP script 
+       https://www.developphp.com/video/JavaScript/File-Upload-Progress-Bar-Meter-Tutorial-Ajax-PHP */ 	   
+}
+</script>
+<h1>File Upload Drop Zone</h1>
+<div id="drop_zone" ondrop="drag_drop(event)" ondragover="return false"></div>
+        
+
+
+
+
+
+ </div>
+    
+        
+
+        
+<!-- TO ZMIEN WYGLAD NIE MUSI NA NIM DZIALAC OPCJA DRAG AND DROP WYSTACZY PRZYCISK DRAG AND DROP BEDZIE W TYMBOXIE 
+WYZEJ-->
+       <input type="file" name="plik" class="PicSelect"/><br/> 
+  
         
     <div class="PicSubmission">
-        <input type="text" name="nazwapliku" class="PicName">
+        Nazwa pliku<input type="text" name="nazwapliku" class="PicName">
         <input type="submit" value="Wyślij"/ class="PicSubmit">
     </div>
     
    </div>
 
   </form>
+  </form>
  </body>
 </html>
-
-
-<?php
-$dataDoda=date('Y-m-d H:i:s');
-echo "$dataDoda";
-
-?>
-
-
-
-
-
-
 
 
