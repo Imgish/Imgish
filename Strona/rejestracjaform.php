10a7aea52bb5289480aa8717183f5cbcc8f3c1fd
Nyny <59801348+NynyNoo@users.noreply.github.com>
2021-01-30 16:32:40 +0100

    Add files via upload

---
+++ Strona/rejestracjaform.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<div class="main2">
+									
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="css/logrej.css"> 
+    </head>
+    <body>
+        
+        
+        <div class="main2">
+									<p class="sign" align="center">Rejestracja</p>
+									<form class="form1" method="POST" action="rejestracja.php" style="text-align:center;">
+									<input class="un" type="text" align="center" name="login" placeholder="login">
+									<input class="pass" type="password" align="center" name="haslo1" placeholder="haslo">
+									<input class="pass" type="password" align="center" name="haslo2" placeholder="powtórz hasło">
+									<input class="pass" type="mail" align="center" name="email" placeholder="e-mail">
+									<input type="submit" value="Utwórz konto" name="rejestruj" class="submit" align="center">
+									</form>
+									</div>
+        
+        
+        
+        
+    </body>
+    </html>
+    
