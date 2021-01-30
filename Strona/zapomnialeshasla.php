943218d33bd05eb2494fb9025eee08b663745f4b
Nyny <59801348+NynyNoo@users.noreply.github.com>
2021-01-30 18:11:20 +0100

    Add files via upload

---
+++ Strona/zapomnialeshasla.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+?>
+<div class="main2">
+									
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="css/PasswordChange.css">
+    
+    <head>
+    </head>
+    
+    <body>
+        
+        <div Form class="Form">
+        <p class="sign" align="center">Przywróć hasło</p>
+			<form class="form1" method="POST" action="zmiana-ustawien.php">
+				<input class="stare_haslo" type="password" align="center" name="Login" placeholder="Login">
+				<br>
+				<input class="nowe_haslo" type="password" align="center" name="Email" placeholder="Email">
+				</br>
+				<input class="powtorz_haslo" type="password" align="center" name="NoweHaslo" placeholder="NoweHaslo">
+				<input class="powtorz_haslo" type="password" align="center" name="Powtórz-NoweHaslo" placeholder="Powtórz-NoweHaslo">
+				<br>
+				<input type="submit" value="Zmień hasło" name="ZmienHaslo" class="submit" align="center">
+				</br>
+		    </form>
+		</div>
+</div>
+        
+    </body>
+</html>
+    
