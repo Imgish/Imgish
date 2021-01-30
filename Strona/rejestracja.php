88b1dc648a240135ba6b44f5e68d5eb7aeb75a5e
Kuba255 <73470071+Kuba255@users.noreply.github.com>
2021-01-30 19:58:12 +0100

    Update rejestracja.php

--- Strona/rejestracja.php
+++ Strona/rejestracja.php
@@ -1,73 +1,89 @@
 <?php
 session_start();
-$con =mysqli_connect('localhost','id15971773_bazaio20test','JakiesLosoweHaslo12!','id15971773_projektio')or die('Brak połączenia z serwerem MySQL.');
-        $login = $_POST['login'];
-        $haslo1 = $_POST['haslo1'];
-        $haslo2 = $_POST['haslo2'];
-        $email = $_POST['email']; 
-        
-$dziala=true;
-if(isset($_POST['login']) and isset($_POST['haslo1']) and isset($_POST['haslo2']) and isset($_POST['email']))
+unset($_SESSION['e_login']);
+$con = mysqli_connect('localhost', 'id15971773_bazaio20test', 'JakiesLosoweHaslo12!', 'id15971773_projektio') or die('Brak połączenia z serwerem MySQL.');
+$login = $_POST['login'];
+$haslo1 = $_POST['haslo1'];
+$haslo2 = $_POST['haslo2'];
+$email = $_POST['email'];
+
+$dziala = true;
+if (isset($_POST['email']))
 {
     //sprawdzanie loginu
-    if((strlen($login)<3) || (strlen($login)>20))
+    if ((strlen($login) < 3) || (strlen($login) > 20))
     {
-        $dziala=false;
-        $_SESSION['e_login']="Login musi posiadać od 3 do 20 znaków.";
-     
+        $dziala = false;
+        $_SESSION['e_login'] = "Login musi posiadać od 3 do 20 znaków.";
     }
-    if(ctype_alnum($login)==false)
+    else
     {
-        $dziala=false;
-        $_SESSION['e_login']="Login musi składać się tylko z liter i cyfr(bez polskich liter).";
-        
+        if (ctype_alnum($login) == false)
+        {
+            $dziala = false;
+            $_SESSION['e_login'] = "Login musi składać się tylko z liter i cyfr(bez polskich liter).";
+        }
+        else
+        {
+            if ((strlen($haslo1) < 5) || (strlen($haslo1) > 20))
+            {
+                $dziala = false;
+                $_SESSION['e_login'] = "Haslo musi posiadać od 5 do 20 znaków.";
+
+            }
+            else
+            {
+                if (ctype_alnum($haslo1) == false)
+                {
+                    $dziala = false;
+                    $_SESSION['e_login'] = "Hasło musi składać się tylko z liter i cyfr(bez polskich liter).";
+
+                }
+                else
+                {
+                    if ($haslo1 != $haslo2)
+                    {
+                        $dziala = false;
+                        $_SESSION['e_login'] = "Hasła muszą być takie same.";
+
+                    }
+                    else
+                    {
+
+                        if ((strlen($email) < 5) || (strlen($email) > 100))
+                        {
+                            $dziala = false;
+                            $_SESSION['e_login'] = "Błędny email.";
+
+                        }
+                        else
+                        {
+
+                            $email2 = filter_var($email, FILTER_SANITIZE_EMAIL);
+                            if ((filter_var($email2, FILTER_VALIDATE_EMAIL) == false) || ($email2 != $email))
+                            {
+                                $dziala = false;
+                                $_SESSION['e_login'] = "Błędny email.";
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-    
-    //sprawdzanie e-mailu
-    $email2=filter_var($email, FILTER_SANITIZE_EMAIL);
-    if((filter_var($email2, FILTER_VALIDATE_EMAIL)==false) || ($email2 != $email))
-    {
-        $dziala=false;
-        $_SESSION['e_login']="Błędny email.";
-        
-    }
-    
-    //sprawdzanie hasła
-    if((strlen($haslo1)<5) || (strlen($haslo1)>20))
-    {
-        $dziala=false;
-        $_SESSION['e_login']="Haslo musi posiadać od 5 do 20 znaków.";
-        
-    }
-    if(ctype_alnum($haslo1)==false)
-    {
-        $dziala=false;
-        $_SESSION['e_login']="Hasło musi składać się tylko z liter i cyfr(bez polskich liter).";
-        
-    }
-    if($haslo1 != $haslo2)
-    {
-        $dziala=false;
-        $_SESSION['e_login']="Hasła muszą być takie same.";
-       
-    }
-    
-}
-else
-{
-        $dziala=false;
-        $_SESSION['e_login']="Nie wypełniono wszystkich pól.";
 }
 
-
-if($dziala==true)
+if ($dziala == true)
 {
-$haslo_szyfr=password_hash($haslo1, PASSWORD_DEFAULT);
-$con->query("INSERT INTO Rejestracja(Login, Haslo, Email)
-VALUES ('$login', '$haslo_szyfr', '$email')");
-$con->close();
-header("Location:reload.php");
+    $haslo_szyfr = password_hash($haslo1, PASSWORD_DEFAULT);
+    $con->query("INSERT INTO Rejestracja(Login, Haslo, Email)
+        VALUES('$login', '$haslo_szyfr', '$email')");
+    $con->close();
+    $_SESSION['e_login'] = "Udało się utworzyć konto.";
+    //header("Location:reload.php");
 }
 header("Location:rejestracjaform.php");
 exit();
 ?>
+
