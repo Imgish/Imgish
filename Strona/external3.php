9cb40447e9965e8a412dbeb07d3d974b625315ba
Matthew <mateusz.kajzer2807pl@gmail.com>
2021-01-30 18:44:46 +0100

    Update external3.php

--- Strona/external3.php
+++ Strona/external3.php
@@ -14,17 +14,17 @@ session_start();
         
         <div Form class="Form">
         <p class="sign" align="center">Zmiana Hasla</p>
-		<form class="form1" method="POST" action="zmiana-ustawien.php">
-			<input class="stare_haslo" type="password" align="center" name="StareHaslo" placeholder="Stare Hasło">
-			<br>
-			<input class="nowe_haslo" type="password" align="center" name="NoweHaslo" placeholder="Nowe Hasło">
-			</br>
-			<input class="powtorz_haslo" type="password" align="center" name="PowtorzNoweHaslo" placeholder="Powtórz Nowe Hasło">
-			<br>
-			<input type="submit" value="Zmień hasło" name="ZmienHaslo" class="submit" align="center">
-			</br>
-		</form>
-	</div>
+			<form class="form1" method="POST" action="zmiana-ustawien.php">
+				<input class="stare_haslo" type="password" align="center" name="StareHaslo" placeholder="Stare Hasło">
+				<br>
+				<input class="nowe_haslo" type="password" align="center" name="NoweHaslo" placeholder="Nowe Hasło">
+				</br>
+				<input class="powtorz_haslo" type="password" align="center" name="PowtorzNoweHaslo" placeholder="Powtórz Nowe Hasło">
+				<br>
+				<input type="submit" value="Zmień hasło" name="ZmienHaslo" class="submit" align="center">
+				</br>
+		    </form>
+		</div>
 </div>
         
     </body>
