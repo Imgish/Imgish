996a0f33e47ee77732affe92b06412e9263b21b7
Matthew <mateusz.kajzer2807pl@gmail.com>
2021-01-30 17:53:01 +0100

    Update external3.php

--- Strona/external3.php
+++ Strona/external3.php
@@ -5,28 +5,28 @@ session_start();
 									
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="css/PasswordChange.css">
+    
     <head>
-        
     </head>
+    
     <body>
         
-        
+        <div Form class="Form">
         <p class="sign" align="center">Zmiana Hasla</p>
-									<form class="form1" method="POST" action="zmiana-ustawien.php">
-									<input class="un" type="password" align="center" name="StareHaslo" placeholder="Stare Hasło">
-									<br>
-									<input class="pass" type="password" align="center" name="NoweHaslo" placeholder="Nowe Hasło">
-									</br>
-									<input class="pass" type="password" align="center" name="PowtorzNoweHaslo" placeholder="Powtórz Nowe Hasło">
-									<br>
-									<input type="submit" value="Zmień hasło" name="ZmienHaslo" class="submit" align="center">
-									</br>
-									</form>
-									</div>
-        
-        
-        
+		<form class="form1" method="POST" action="zmiana-ustawien.php">
+			<input class="stare_haslo" type="password" align="center" name="StareHaslo" placeholder="Stare Hasło">
+			<br>
+			<input class="nowe_haslo" type="password" align="center" name="NoweHaslo" placeholder="Nowe Hasło">
+			</br>
+			<input class="powtorz_haslo" type="password" align="center" name="PowtorzNoweHaslo" placeholder="Powtórz Nowe Hasło">
+			<br>
+			<input type="submit" value="Zmień hasło" name="ZmienHaslo" class="submit" align="center">
+			</br>
+		</form>
+	</div>
+</div>
         
     </body>
-    </html>
+</html>
     
