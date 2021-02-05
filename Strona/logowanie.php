4e3e6c85bf6bdd4520269c2850e3735e0566a00b
Kuba255 <73470071+Kuba255@users.noreply.github.com>
2021-02-05 09:54:53 +0100

    Update logowanie.php

--- Strona/logowanie.php
+++ Strona/logowanie.php
@@ -1,31 +1,26 @@
 <?php
 session_start();
+unset($_SESSION['e_login']);
 $con =mysqli_connect('localhost','id15971773_bazaio20test','JakiesLosoweHaslo12!','id15971773_projektio')or die('Brak połączenia z serwerem MySQL.');
-        $login = $_POST['login'];
-        $haslo = $_POST['haslo'];
-
-        // sprawdzamy czy login i hasło są dobre
+$login = $_POST['login'];
+$haslo = $_POST['haslo'];
 $query = "SELECT Login,Haslo FROM Rejestracja WHERE Login='$login'";
 $data=mysqli_query($con,$query);
-        
-if (mysqli_num_rows($data)>0)
+$wiersz = $data->fetch_assoc();     
+if (mysqli_num_rows($data)>0 && password_verify($haslo, $wiersz['Haslo']))
 {
-    $wiersz = $data->fetch_assoc();
-    
-    if(password_verify($haslo, $wiersz['Haslo']))
-    {
+    //if(password_verify($haslo, $wiersz['Haslo']))
     $_SESSION['zalogowany'] = true;
     $_SESSION['login'] = $login;
-    }
-    else
-    {
-       echo "niedziala";
-   
-    }
+    header("Location:reload.php");
+}
+else
+{
+    //echo 'test';
+    $_SESSION['e_login'] = "Nieprawidłowy login lub hasło.";
+    header("Location:logowanieform.php");
 }
 $con->close(); 
-
-
-header("Location:reload.php");
+//header("Location:reload.php");
 exit();
 ?>
