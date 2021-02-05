424766f986b7183a167ac0ee3d0d529152c91b19
Kuba255 <73470071+Kuba255@users.noreply.github.com>
2021-02-05 11:45:08 +0100

    Update Zdjecia-uzytkownika.php

--- Strona/Zdjecia-uzytkownika.php
+++ Strona/Zdjecia-uzytkownika.php
@@ -1,13 +1,22 @@
-
+<?php
+session_start();
+?>
 <html>
- <link rel="stylesheet" href="css/PicUpload.css">      
- <body>
+<link rel="stylesheet" href="css/PicUpload.css">      
+<body>
 <form action="wgrywanie.php" method="POST" ENCTYPE="multipart/form-data"> 
     <div class="body_dropzone">
         <div class="PicChose">
 
 <h1>Dodaj zdjęcie na serwer</h1>
     <div class="Form">
+    <p class="RejError">
+    <?php  
+        if (isset($_SESSION['e_zdjęcie'])) 
+        {
+        echo $_SESSION['e_zdjęcie'];
+        }
+    ?></p>
         <input type="file" name="plik" class="PicSelect"  style="margin-bottom: 2%;"> 
 
         <div class="PicSubmission">
@@ -17,11 +26,8 @@
     
     </div>
     
- </div>
+        </div>
     </div>
-  </form>
-  </form>
- </body>
+</form>
+</body>
 </html>
-
-
