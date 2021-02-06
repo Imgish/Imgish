462047cba8650d36e5b5da99d01e3218dca142f6
Nyny <59801348+NynyNoo@users.noreply.github.com>
2021-02-06 16:11:34 +0100

    Update lapka.php

--- Strona/lapka.php
+++ Strona/lapka.php
@@ -2,24 +2,28 @@
 session_start();
 $tytul = $_POST['Link'];
 $pomoc=$_SESSION['login'];
-echo $tytul;
-echo $pomoc;
 $con =mysqli_connect('localhost','id15971773_bazaio20test','JakiesLosoweHaslo12!','id15971773_projektio')or die('Brak połączenia z serwerem MySQL.');
-$query = "SELECT * FROM Lapka WHERE TytulObrazu = '$tytu' AND Login = '$pomoc'" ;
+
+
+
+
+$query = "SELECT * FROM Lapka WHERE TytulObrazu = '$tytul' AND Login = '$pomoc'" ;
 $data=mysqli_query($con,$query);
 if (mysqli_num_rows($data)>0)
 {
-    echo "dales juz lapke";
+  
+     $con->query("DELETE FROM Lapka WHERE TytulObrazu = '$tytul' AND Login = '$pomoc'");
+     $con->query("UPDATE Obrazy SET Lapka = (Lapka - 1) WHERE Nazwa = '$tytul'");
+    
 }
 else
 {
-    
-    
     $con->query("INSERT INTO Lapka(Login,TytulObrazu) VALUES
         ('$pomoc','$tytul')");
-    echo "dziala";
+       $con->query("UPDATE Obrazy SET Lapka = (Lapka + 1) WHERE Nazwa = '$tytul'");
 }
 $con->close(); 
+header("Location: Najnowsze2.php");
 
 
 
